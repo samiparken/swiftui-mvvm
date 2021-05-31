e537a15bb5fa989bb5e9ffb2c8a6367b9e87dc06
Han-Saem Park <samiparken@gmail.com>
2021-05-31 12:15:59 +0200

    add navigationView

--- swiftui-mvvm/ContentView.swift
+++ swiftui-mvvm/ContentView.swift
@@ -7,28 +7,34 @@ struct Item: Identifiable {
 
 struct ContentView: View {
     //MARK: - PROPERTIES
+    /* @State property wrapper to allow us to modify values inside a struct */
     @State var items = [Item(id:UUID(),name:"First"),
-                 Item(id:UUID(),name:"Second"),
-                 Item(id:UUID(),name:"Third")]
-    
-    /*
-     @State property wrapper to allow us to modify values inside a struct,
-     */
+                        Item(id:UUID(),name:"Second"),
+                        Item(id:UUID(),name:"Third")]
     
     
     //MARK: - BODY
     
     var body: some View {
-        List {
-            ForEach(items) {item in
-                Text(item.name)
-            }
-            .onDelete(perform: { indexSet in
-                for index in indexSet {
-                    items.remove(at:index)
+        NavigationView {
+            List {
+                ForEach(items) {item in
+                    Text(item.name)
                 }
+                .onDelete(perform: { indexSet in
+                    for index in indexSet {
+                        items.remove(at:index)
+                    }
+                })
+                .onMove(perform: { indices, newOffset in
+                    items.move(fromOffsets: indices, toOffset: newOffset)
+                })
+            }//:List
+            .navigationBarTitle(Text("Todo's"), displayMode: .large)
+            .toolbar(content:{
+                EditButton()
             })
-        }
+        }//:NavigationView
     }
 }
 
