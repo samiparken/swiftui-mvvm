ef4fe5d4994d90db4ccd730a39324ac2ac79a6d3
Han-Saem Park <samiparken@gmail.com>
2021-05-31 11:27:46 +0200

    Initial Commit

---
+++ swiftui-mvvm/swiftui_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mvvmApp.swift
+//  swiftui-mvvm
+//
+//  Created by Sam on 2021-05-31.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
