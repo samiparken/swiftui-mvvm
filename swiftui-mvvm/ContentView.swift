8976df24ea21af7bb1ecb6abfc1359ec0971d827
Han-Saem Park <samiparken@gmail.com>
2021-05-31 12:03:47 +0200

    add List view

--- swiftui-mvvm/ContentView.swift
+++ swiftui-mvvm/ContentView.swift
@@ -1,19 +1,33 @@
-//
-//  ContentView.swift
-//  swiftui-mvvm
-//
-//  Created by Sam on 2021-05-31.
-//
-
 import SwiftUI
 
+struct Item: Identifiable {
+    var id: UUID
+    var name: String
+}
+
 struct ContentView: View {
+    //MARK: - PROPERTIES
+    var items = [Item(id:UUID(),name:"First"),
+                 Item(id:UUID(),name:"Second"),
+                 Item(id:UUID(),name:"Third")]
+    
+    //MARK: - BODY
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            ForEach(items) {item in
+                Text(item.name)
+            }
+            .onDelete(perform: { indexSet in
+                for index in indexSet {
+                    items.remove(at:index)
+                }
+            })
+        }
     }
 }
 
+//MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
